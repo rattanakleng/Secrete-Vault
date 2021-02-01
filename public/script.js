b5fcd021f8d8236129d4098d2f94b3a5cc86b5b7
Rattanak Leng <rleng@live.com>
2021-02-01 11:32:38 -0800

    add delete and update function, add update-password page

--- public/script.js
+++ public/script.js
@@ -1,28 +1,118 @@
-$(".input").on("submit", function (event) {
+$(document).ready(function () {
+    $(".input").on("submit", function (event) {
 
-    event.preventDefault();
+        event.preventDefault();
+
+        let newPassword = {
+            name: $("#input-name").val().trim(),
+            link: $("#input-link").val().trim(),
+            username: $("#input-username").val().trim(),
+            password: $("#input-password").val().trim(),
+            description: $("#input-description").val().trim()
+        }
+
+        console.log(newPassword);
+
+        $.ajax("/api/password/post", {
+            type: "POST",
+            data: newPassword
+        })
+            .then(function () {
+                console.log("Password is added");
+                location.reload();
+            })
+        // Another way to write using build in jquery 
+        // $.post("/api/posts",newPassword,function(){
+        //     console.log("Password is added");
+        // })
+    });
 
 
-    let newPassword = {
-        name: $("#input-name").val().trim(),
-        link: $("#input-link").val().trim(),
-        username: $("#input-username").val().trim(),
-        password: $("#input-password").val().trim(),
-        description: $("#input-description").val().trim()
-    }
+    // Delete a password
+    $(document).on("click", "#delete-password", function (event) {
+        event.preventDefault();
+        console.log(event.target);
+        const dataId = $(event.target).attr("data-id");
+        $.ajax({
+            method: "DELETE",
+            url: "/api/password/delete/" + dataId
+        }).then(function () {
+            location.reload("/view-password");
+        })
+    });
 
-    console.log(newPassword);
+    // Updating a password
 
-    // $.ajax("/api/posts", {
-    //     type: "POST",
-    //     data: newPassword
-    // })
-    //     .then(function () {
-    //         console.log("Password is added");
-    //         // location.reload();
+    $(document).on("click", "#update-password", function (event) {
+
+        const dataId = $(this).attr("data-id");
+        location.href = "/update-password/" + dataId;
+
+    })
+
+    // Call Api to update password
+
+    $(".update").on("submit", function (event) {
+
+        event.preventDefault();
+
+        let newPassword = {
+            name: $("#input-name").val().trim(),
+            link: $("#input-link").val().trim(),
+            username: $("#input-username").val().trim(),
+            password: $("#input-password").val().trim(),
+            description: $("#input-description").val().trim()
+        }
+
+
+        let id = $("#updateId").val();
+        // alert(id);
+        console.log(id);
+        $.ajax("/api/password/update/" + id, {
+            type: "PUT",
+            data: newPassword
+        })
+            .then(function () {
+                console.log("Password is added");
+                location.reload("/view-password");
+            })
+
+
+
+
+        // Another way to write using build in jquery 
+        // $.post("/api/posts",newPassword,function(){
+        //     console.log("Password is added");
+        // })
+    });
+
+
+
+
+
+
+
+    // function handleDeletePassword() {
+    //     let currentPassword = $(this)
+    //     .parent()
+    //     .parent()
+    //     .data("passwords");
+    //     deletePassword(currentPassword.id);
+    // }
+    //Function does an API call to delete password
+    // $("#edit-password").on("click", function() {
+
+    //     $.ajax({
+    //         method: "DELETE",
+    //         url: "/api/passwords/2"
+    //     }).then(function() {
+    //         alert("password is deleted");
     //     })
 
-        $.post("/api/posts",newPassword,function(){
-            console.log("Password is added");
-        })
 })
+
+
+
+
+
+
